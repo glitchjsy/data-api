98ea52caa1cfa04d1acff11348453ff8398fd5a0
Luke <luke.wyatt21@gmail.com>
2024-06-18 10:22:53 +0100

    Require user agent to be specified in requests

--- src/utils/request-interceptor.ts
+++ src/utils/request-interceptor.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
 import log from "./log";
+import { ErrorCode } from "./errors/ErrorCode";
 
 export default function requestInterceptor(req: Request, res: Response, next: Function) {
+    if (!req.headers["User-Agent"]) {
+        return res.status(400).json({
+            error: ErrorCode.MISSING_USER_AGENT,
+            status: 400,
+            message: "Missing user agent"
+        });
+    }
     res.json = (data: any): any => {
         res.set("Content-Type", "application/json");
         res.send(JSON.stringify(data, null, req.query.pretty === "true" ? 2 : 0));
