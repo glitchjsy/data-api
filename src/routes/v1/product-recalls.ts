1a808fbb41c90a5922b4f78d19f3ce3b1a4edc28
Luke <luke.wyatt21@gmail.com>
2024-06-18 10:08:36 +0100

    Add pagination to product recalls route

--- src/routes/v1/product-recalls.ts
+++ src/routes/v1/product-recalls.ts
@@ -2,16 +2,34 @@ import apicache from "apicache";
 import { Router } from "express";
 import mysql from "../../mysql";
 import errorHandler from "../../utils/error-handler";
-import { onlyApiSuccess } from "../../utils/utils";
+import { onlyApiSuccess, queryParamNumber } from "../../utils/utils";
 
 const router = Router();
 const cache = apicache.middleware;
 
 router.get("/",  async (req, res) => {
+    const page = queryParamNumber(req.query.page) ?? 1;
+    const limit = queryParamNumber(req.query.limit) ?? 50;
+    const offset = (page - 1) * limit;
+
+    const countResult = await mysql.execute("SELECT COUNT(*) AS count FROM productRecalls");
+    const totalItems = countResult[0].count;
+    const totalPages = Math.ceil(totalItems / limit);
+    
     const results = await mysql.execute(`
         SELECT * FROM productRecalls
-    `);
-    return res.json({ results });
+        LIMIT ?
+        OFFSET ?
+    `, [limit, offset]);
+    return res.json({
+        pagination: {
+            page,
+            limit,
+            totalPages,
+            totalItems
+        },
+        results
+    });
 });
 
 router.use(errorHandler);
