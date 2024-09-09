6e19e247c71db786ea885d7492f023e388860620
Luke <luke.wyatt21@gmail.com>
2024-09-09 10:14:32 +0100

    new chart data

--- src/routes/v1/charts-temp.ts
+++ src/routes/v1/charts-temp.ts
@@ -4,6 +4,7 @@ import { onlyApiSuccess } from "../../utils/utils";
 
 import busPassengersJson from "../../../static/bus-passengers-weekly.json";
 import roadTrafficJson from "../../../static/road-traffic-weekly.json";
+import drivingTestJson from "../../../static/practical-driving-test-results.json";
 
 const router = Router();
 const cache = apicache.middleware;
@@ -15,5 +16,10 @@ router.get("/bus-passengers", cache("1 hour", onlyApiSuccess), async (req, res) 
 router.get("/road-traffic", cache("1 hour", onlyApiSuccess), async (req, res) => {
     return res.json(roadTrafficJson);
 });
+
+router.get("/driving-test-results", cache("1 hour", onlyApiSuccess), async (req, res) => {
+    return res.json(drivingTestJson);
+});
+
 
 export default router;
